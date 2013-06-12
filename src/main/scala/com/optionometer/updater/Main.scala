0f043deb0944a967d25edf8647538dbd53f1e313
Matthew Frost <mf@matthewfrost.com>
2013-06-11 21:40:51 -0400

    socket boilerplate

--- src/main/scala/com/optionometer/updater/Main.scala
+++ src/main/scala/com/optionometer/updater/Main.scala
@@ -4,7 +4,7 @@ import org.slf4j.{Logger, LoggerFactory}
 
 object Main extends App {
   
-  private def Log: Logger = LoggerFactory.getLogger(this.getClass)
+  private lazy val Log: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def main(args: Array[String]): Unit = {
     Log.info("*** Optionometer Quote Updater Started ***")
--- src/main/scala/com/optionometer/updater/QuoteImporter.scala
+++ src/main/scala/com/optionometer/updater/QuoteImporter.scala
@@ -1,18 +1,42 @@
 package main.scala.com.optionometer.updater
 
+import java.io._
+import java.net.{Socket, ServerSocket}
+import scala.io.Source
 import org.slf4j.{Logger, LoggerFactory}
 
 object QuoteImporter {
   
-  private val Username = sys.env("FIX_USERNAME")
-  private val Password = sys.env("FIX_PASSWORD")
-  private val Host = sys.env("FIX_IP")
-  private val Port = sys.env("FIX_PORT")
+  private val username = sys.env("FIX_USERNAME")
+  private val password = sys.env("FIX_PASSWORD")
+  private val host = sys.env("FIX_IP")
+  private val port: Int = sys.env("FIX_PORT").toInt
   
-  private def Log: Logger = LoggerFactory.getLogger(this.getClass)
+//  private lazy val socket = new Socket(host, port)
+  private lazy val socket = new Socket("127.0.0.1", 1811)
+  private lazy val out = new OutputStreamWriter(socket.getOutputStream)
   
-  def begin = {
+  private lazy val Log: Logger = LoggerFactory.getLogger(this.getClass)
+  
+  def begin {
     Log.info("Updating Quotes...")
+    logIn
+    while (socket.isConnected) {
+      Source.fromInputStream(socket.getInputStream).getLines.foreach(f=>println(f))
+    }
+    out.close
+    socket.close
+    Log.info("Connection Closed!")
   }
-
+  
+  private def logIn {
+    Log.info("Logging into FIX connection...")
+    sendMessage("L|100=" + username + ";101=" + password + "\n")
+  }
+  
+  private def sendMessage(msg: String) {
+    out.write(msg)
+    out.flush
+  }
+  
 }
