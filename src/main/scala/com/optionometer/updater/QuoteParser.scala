ea3a3357d85e416ac6955ac1d9c6c3b021418133
Matthew Frost <mf@matthewfrost.com>
2013-06-15 14:46:13 -0400

    added FIX field constants

--- src/main/scala/com/optionometer/updater/QuoteParser.scala
+++ src/main/scala/com/optionometer/updater/QuoteParser.scala
@@ -8,13 +8,20 @@ object QuoteParser extends Fields {
   private lazy val log: Logger = LoggerFactory.getLogger(this.getClass)
   
   def parse(msg: String) {
-//    println(msg)	//DELME
     msg match {
       case m if msg.startsWith("G") => log.info("Successfully logged into quote server '{}'", mapFields(m).get(QUOTE_SERVER).getOrElse(""))    	
       case m if msg.startsWith("D") => log.info("Login denied: {}", mapFields(m).get(LOGIN_REASON).getOrElse("Reason unknown"))
-//      case m if msg.startsWith("1") => println("LOGGED IN")
+      case m if msg.startsWith("1") => parseLevelOne(m)
       case _ => None 
     }
+  }
+  
+  private def parseLevelOne(msg: String) {
+    println(msg)	//DELME
+  }
+  
+  private def parseOptionChain(msg: String) {
+    
   }
   
   private def mapFields(msg: String): Map[Int, Any] = {
@@ -40,6 +47,26 @@ object QuoteParser extends Fields {
 }
 
 trait Fields {
-  val LOGIN_REASON = 103
-  val QUOTE_SERVER = 8055
+
+  /* 		Account		  */
+  val LOGIN_REASON 	= 	103
+  val QUOTE_SERVER 	= 	8055
+  
+  /* 	Stocks & Options  */
+  val SYMBOL		= 	1003
+  val LAST			= 	2002
+  val BID			= 	2003
+  val ASK			= 	2004
+  val VOLUME		= 	2012
+  val TIMESTAMP		= 	2014
+  val DATE			= 	2015
+  
+  /* 	  Options only 	  */
+  val UNDERLIER		= 	2034
+  val STRIKE_PRICE	= 	2035
+  val EXP_MONTH		= 	2036
+  val OPEN_INTEREST	= 	2037
+  val PUT_CALL		= 	2038
+  val EXP_YEAR		= 	2040
+  
 }
