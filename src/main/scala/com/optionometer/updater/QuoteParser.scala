8da7639b4f69aeae317f8cc07afc1f5b947bf16a
Matthew Frost <mf@matthewfrost.com>
2013-10-05 18:13:06 -0400

    ignore weekly contracts

--- src/main/scala/com/optionometer/updater/QuoteParser.scala
+++ src/main/scala/com/optionometer/updater/QuoteParser.scala
@@ -1,6 +1,7 @@
 package com.optionometer.updater
 
 import java.text.{ParseException, SimpleDateFormat}
+import java.util.{Calendar, GregorianCalendar}
 import scala.collection.immutable.HashMap
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -9,7 +10,7 @@ object QuoteParser extends Fields with safeCast {
   private lazy val dbh = DBHandler
   private lazy val log: Logger = LoggerFactory.getLogger(this.getClass)
   
-  def parse(msg: String): Unit = msg match {
+  def parse(msg: String) = msg match {
     case m if msg.startsWith("G") => log.info("Successfully logged into quote server '{}'", mapFields(m).get(QUOTE_SERVER).getOrElse(""))    	
     case m if msg.startsWith("D") => log.info("Login denied: {}", mapFields(m).get(LOGIN_REASON).getOrElse("Reason unknown"))
     case m if msg.startsWith("1") => parseLevelOne(m)
@@ -25,7 +26,7 @@ object QuoteParser extends Fields with safeCast {
     if (fieldMap.contains(LAST)) {
       val sym = fieldMap(SYMBOL)
       val last = toBigDecimal(fieldMap.get(LAST).getOrElse("0"))
-      val timestamp = getUNIXTime(fieldMap.get(TIMESTAMP).getOrElse(""), fieldMap.get(DATE).getOrElse(""))
+      val timestamp = unixTime(fieldMap.get(TIMESTAMP).getOrElse(""), fieldMap.get(DATE).getOrElse(""))
       dbh.updateStock(new StockInfo(sym, last, timestamp))
     }
   }
@@ -33,19 +34,28 @@ object QuoteParser extends Fields with safeCast {
   private def parseOptionChain(msg: String) {
     val fieldMap = mapFields(msg)
     /* 
-     * The bulk of option updates recieved are just changes to the bid or ask size. Ignoring these.
-     * Every option response includes underlier symbol, option symbol, timestamp, and date.
-     * Only further processing responses containing updates to the bid or ask (and the initial response
-     * containing all relevant fields)
+     * Ignoring option updates that are just changes to the bid or ask size as well as weekly contracts
+     * (expires on Friday instead of Saturday). Every option response includes underlier symbol, 
+     * option symbol, timestamp, and date.Only further processing responses containing updates 
+     * to the bid or ask (and the initial response containing all relevant fields)
      */
     if (fieldMap.contains(ASK) || fieldMap.contains(BID) || fieldMap.size > 6) {
-      val timestamp = getUNIXTime(fieldMap.get(TIMESTAMP).getOrElse(""), fieldMap.get(DATE).getOrElse(""))
-      val option = OptionInfo(timestamp, fieldMap)
-      dbh.updateOption(option)
+      val timestamp = unixTime(fieldMap.get(TIMESTAMP).getOrElse(""), fieldMap.get(DATE).getOrElse(""))
+	  val option = OptionInfo(timestamp, fieldMap)
+	  if (isNotWeekly(option)) {
+	      dbh.updateOption(option)
+      }
     }
   }
   
-  private def getUNIXTime(time: String, date: String): Long = {
+  private def isNotWeekly(option: OptionInfo): Boolean = {
+    val opt = option.fieldMap
+    val cal = new GregorianCalendar(toInt(opt(EXP_YEAR)), toInt(opt(EXP_MONTH))-1, toInt(opt(EXP_DAY)))
+    return cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+  }
+  
+  
+  private def unixTime(time: String, date: String): Long = {
     val dateString: String = time +"-"+ date
     val dateFormat = "HH:mm:ss-MM/dd/yy"
     val df = new SimpleDateFormat(dateFormat)
--- src/main/scala/com/optionometer/updater/SafeCast.scala
+++ src/main/scala/com/optionometer/updater/SafeCast.scala
@@ -1,6 +1,9 @@
 package com.optionometer.updater
 
 trait safeCast {
+  
+  def toInt(any: Any): Int = toInt(any.toString)
+  
   def toInt(str: String): Int = {
     try {
       return str.toInt
